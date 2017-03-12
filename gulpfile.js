2ec485eb63ba27f27c78873934c11f392713202b
Jay Jamero <jayjamero@gmail.com>
2017-03-12 10:27:47 +0800

    space fix

--- gulpfile.js
+++ gulpfile.js
@@ -43,15 +43,15 @@ const mapError = (err) => {
   if (err.fileName) {
     // Regular error
     gutil.log(`${chalk.red(err.name)
-       }: ${chalk.yellow(err.fileName.replace(`${__dirname}/src/js/`, ''))
-       }: Line ${chalk.magenta(err.lineNumber)
-       } & Column ${chalk.magenta(err.columnNumber || err.column)
-       }: ${chalk.blue(err.description)}`);
+      }: ${chalk.yellow(err.fileName.replace(`${__dirname}/src/js/`, ''))
+      }: Line ${chalk.magenta(err.lineNumber)
+      } & Column ${chalk.magenta(err.columnNumber || err.column)
+      }: ${chalk.blue(err.description)}`);
   } else {
     // Browserify error..
     gutil.log(`${chalk.red(err.name)
-       }: ${
-       chalk.yellow(err.message)}`);
+      }: ${
+      chalk.yellow(err.message)}`);
   }
 };
 
